785e9ffc2ee8193ec021a4718c04980eeb80ea5a
zliu1Charlotte <48894053+zliu1Charlotte@users.noreply.github.com>
2021-11-16 16:52:50 -0500

    Add files via upload

--- FAME/cw-coaes/coAES.c
+++ FAME/cw-coaes/coAES.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "../trigger/FAME_trigger.h"
+#include "../uart/FAME_uart.h"
 
 //int i, read_data_0, read_data_1, read_data_2, read_data_3;
 
@@ -10,93 +11,73 @@
 void coAES_ecb_enc(int* key_enc, volatile int* volatile data_enc, volatile int* volatile ct) {
 	int i;
     aes_ptr = AES_BASE_ADDR;
+
 	aes_ptr[0] = key_enc[0];	//key_encrypt_0
 	aes_ptr[1] = key_enc[1];	//key_encrypt_1
 	aes_ptr[2] = key_enc[2];	//key_encrypt_2
 	aes_ptr[3] = key_enc[3];	//key_encrypt_3
-    for (int i = 0; i<5; i++) {
+    
 			aes_ptr[8]  = ((int *) data_enc)[0] ^ aes_ptr[16];
 			aes_ptr[9]  = ((int *) data_enc)[1] ^ aes_ptr[17];
 			aes_ptr[10] = ((int *) data_enc)[2] ^ aes_ptr[18];
 			aes_ptr[11] = ((int *) data_enc)[3] ^ aes_ptr[19];
-			aes_ptr[32] = 0;
-			aes_ptr[32] = 1;
+
+			//for (int i = 0; i < 4; i++){printf("%x", data_enc[i]);}
+			//printf("\n");
+
+			
+			pio[1] = 0x1;
+
+			aes_ptr[32] = 0x00000000;	//control_register
+			aes_ptr[32] = 0x00000001;	//control_register
+			
+			while(aes_ptr[33]==0); // wait for 
+
+			pio[1] = 0x0;
+
 			((int *) ct) [0] = aes_ptr[24];
 			((int *) ct) [1] = aes_ptr[25];
 			((int *) ct) [2] = aes_ptr[26];
 			((int *) ct) [3] = aes_ptr[27];
-			// uncomment for random encryptions
-			memcpy(data_enc, ct, 16);
-		}
 
-
-	////// aes_ptr[0] = key_enc[0];	//key_encrypt_0
-	////// aes_ptr[1] = key_enc[1];	//key_encrypt_1
-	////// aes_ptr[2] = key_enc[2];	//key_encrypt_2
-	////// aes_ptr[3] = key_enc[3];	//key_encrypt_3
-	
-	//aes_ptr[16] = init_vector_enc[0];	//init_vector_encrypt_0
-	//aes_ptr[17] = init_vector_enc[1];	//init_vector_encrypt_1
-	//aes_ptr[18] = init_vector_enc[2];	//init_vector_encrypt_2
-	//aes_ptr[19] = init_vector_enc[3];	//init_vector_encrypt_3
-	
-    //input data xor with init_vector because we cannot set init_vector to 0
-	//////aes_ptr[8] = data_enc[0] ^ aes_ptr[16];	//data_encrypt_0
-	//////aes_ptr[9] = data_enc[1] ^ aes_ptr[17];	//data_encrypt_1
-	//////aes_ptr[10] = data_enc[2] ^ aes_ptr[18];	//data_encrypt_2
-	//////aes_ptr[11] = data_enc[3] ^ aes_ptr[19];	//data_encrypt_3
-	
-	////// SCA_TRIGGER();
-	////// aes_ptr[32] = 0x00000001;	//control_register
-	////// //for(i = 0; i < 10; i++); 
-	////// aes_ptr[32] = 0x00000000;	//control_register
-	
-	//for(i = 0; i < 10; i++); 
-//	while(aes_ptr[33] != 0x00000001);
-
-	////// ct[0] = aes_ptr[24];
-	////// ct[1] = aes_ptr[25];
-	////// ct[2] = aes_ptr[26];
-	////// ct[3] = aes_ptr[27];
-
-//	ct[0] = aes_ptr[24];
-//	ct[1] = aes_ptr[25];
-//	ct[2] = aes_ptr[26];
-//	ct[3] = aes_ptr[27];
-	//ct[3] = aes_ptr[27];
-	//ct[2] = aes_ptr[26];
-	//ct[1] = aes_ptr[25];
-	//ct[0] = aes_ptr[24];
-
-/*	int temp = rand()%1000;
-	ct[0] = temp;
-	ct[1] = temp-1;
-	ct[2] = temp-2;
-	ct[3] = temp-3;
-*/	
-	//printf("AES output = %08x \n", ct[0]);
-	//printf("AES output = %08x \n", ct[1]);
-	//printf("AES output = %08x \n", ct[2]);
-	//printf("AES output = %08x \n", ct[3]);
-/*
-	aes_ptr[32] = 0x00000001;	//control_register
-	//for(i = 0; i < 10; i++); 
-	aes_ptr[32] = 0x00000000;	//control_register
-	
-	//for(i = 0; i < 1000; i++); 
-	while(aes_ptr[33] != 0x00000001);
-
-	ct[0] = aes_ptr[24];
-	ct[1] = aes_ptr[25];
-	ct[2] = aes_ptr[26];
-	ct[3] = aes_ptr[27];
-*/
-	
-//	printf("AES output = %08x \n", ct[0]);
-//	printf("AES output = %08x \n", ct[1]);
-//	printf("AES output = %08x \n", ct[2]);
-//	printf("AES output = %08x \n", ct[3]);
+			// memcpy(data_enc, ct, 16);
 }
+
+// void coAES_ecb_enc(int* key_enc, volatile int* volatile data_enc, volatile int* volatile ct) {
+// 	int i;
+//     aes_ptr = AES_BASE_ADDR;
+
+//     // for (int i = 0; i < 4; i++){printf("%x", data_enc[i]);} 
+//     // printf("\n");
+
+// 	aes_ptr[0] = key_enc[0];	//key_encrypt_0
+// 	aes_ptr[1] = key_enc[1];	//key_encrypt_1
+// 	aes_ptr[2] = key_enc[2];	//key_encrypt_2
+// 	aes_ptr[3] = key_enc[3];	//key_encrypt_3
+	
+// 	//aes_ptr[16] = init_vector_enc[0];	//init_vector_encrypt_0
+// 	//aes_ptr[17] = init_vector_enc[1];	//init_vector_encrypt_1
+// 	//aes_ptr[18] = init_vector_enc[2];	//init_vector_encrypt_2
+// 	//aes_ptr[19] = init_vector_enc[3];	//init_vector_encrypt_3
+	
+//     //input data xor with init_vector because we cannot set init_vector to 0
+// 	aes_ptr[8] = data_enc[0] ^ aes_ptr[16];	//data_encrypt_0
+// 	aes_ptr[9] = data_enc[1] ^ aes_ptr[17];	//data_encrypt_1
+// 	aes_ptr[10] = daEducationta_enc[2] ^ aes_ptr[18];	//data_encrypt_2
+// 	aes_ptr[11] = data_enc[3] ^ aes_ptr[19];	//data_encrypt_3
+	
+// 	aes_ptr[32] = 0x00000001;	//control_register
+// 	//for(i = 0; i < 10; i++); 
+// 	aes_ptr[32] = 0x00000000;	//control_register
+	
+// 	//for(i = 0; i < 10000; i++); 
+// //	while(aes_ptr[33] != 0x00000001);
+
+// 	ct[0] = aes_ptr[24];
+// 	ct[1] = aes_ptr[25];
+// 	ct[2] = aes_ptr[26];
+// 	ct[3] = aes_ptr[27];
+// }
 
 //ECB DECRYPT
 void coAES_ecb_dec(int* key_dec, volatile int* volatile data_dec, volatile int* volatile pt) {
@@ -309,42 +290,64 @@ void coAES_cbc_dec(int* key_dec, volatile int* volatile data_dec, volatile int* 
     }
 }
 
+#define RXBUFFERLENGTH 16
+#define AES128LENGTHINBYTES 16
+uint8_t rxBuffer[RXBUFFERLENGTH] = { };
+
 int main(){
+	init_uart();
+	init_gpio(); 
+	set_baud(38400);
+	// 38400 baud rate for 4mhz
+	// 19200 baud rate for 8mhz
+	// 9600 baud rate for 16mhz
 
-	int input[16] = { 0xcafebabe, 0xdeadbeef, 0xdeadbeef, 0x65236723 };
- 	int key[16] = { 0x54eeff11, 0x2132eb43, 0xbbaaff23, 0x98541212};
-	int output[16];
-	init_gpio(); //for trigge
+    uint8_t cmd;
 
-	while(1) {
-	// for (int i = 0; i < 4; i++){
-	// 	input[i] = 0x01 + input[i];
-	SCA_TRIGGER();
- 	//coAES_ecb_enc(key,input,output);
- 	coAES_ecb_dec(key,input,output);
+    int keyAES[4] = { 0x54eeff11, 0x2132eb43, 0xbbaaff23, 0x98541212 };
+    int output[4];
+    for (int i = 0; i < RXBUFFERLENGTH; i++) rxBuffer[i] = 0;
+
+while(1) {
+
+    	//while( get_c() != 'R');
+    	//put_c_int('A');
+	//put_c('A');
+	//put_c_int('B');
+	cmd = get_c();
+	if (cmd == 'P'){
+		get_bytes(16, rxBuffer); // get 16 byte plaintext 
+		coAES_ecb_enc(keyAES,rxBuffer,output);
+		send_bytes(16,output);
+
+		//for (int i = 0; i < 4; i++){printf("%x", output[i]);}
+		//printf("\n\n");
+
+		}
 	}
+}
 
 
-//   //coaes  encryption
-//   printf("COAES ECB ENCRYPTION\n");
-//   int key[16] = { 0x54eeff11, 0x2132eb43, 0xbbaaff23, 0x98541212};
-//   printf("128-bit key: ");
-//   for (int i = 0; i < 4; i++){
-//   printf("%x", key[i]);
-//   } 
+  // //coaes  encryption
+  // // printf("COAES ECB ENCRYPTION\n");
+  // // int key[16] = { 0x54eeff11, 0x2132eb43, 0xbbaaff23, 0x98541212};
+  // printf("128-bit key: ");
+  // for (int i = 0; i < 4; i++){
+  // printf("%x", key[i]);
+  // } 
 
-//   int input[16] = { 0xcafebabe, 0xdeadbeef, 0xdeadbeef, 0x65236723 };
-//   printf("\n128-bit input: ");
-//   for (int i = 0; i < 4; i++){
-//     printf("%x", input[i]);
-//   }
+  // int input[16] = { 0xcafebabe, 0xdeadbeef, 0xdeadbeef, 0x65236723 };
+  // printf("\n128-bit input: ");
+  // for (int i = 0; i < 4; i++){
+  //   printf("%x", input[i]);
+  // }
 
-//   int output[16];
-//   coAES_ecb_enc(key,input,output);
-//   printf("\n128-bit output: ");
-//   for (int i = 0; i < 4; i++){
-//     printf("%x", output[i]);
-//   }
+  // int output[16];
+  // coAES_ecb_enc(key,input,output);
+  // printf("\n128-bit output: ");
+  // for (int i = 0; i < 4; i++){
+  //   printf("%x", output[i]);
+  // }
 
 //   printf("\n\nCOAES ECB DECRYPTION");
 //   printf("\n128-bit key: ");
@@ -405,7 +408,3 @@ int main(){
 //     printf("%x", de_out0[i]);
 //   } 
 //   printf("\n");
-
-
-  return 0;
-}
